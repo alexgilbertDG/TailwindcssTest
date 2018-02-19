bcd5958a216ebcbdf5c85238367388ff0c4fbf5a
Alex Gilbert <alexgilbertdesign@gmail.com>
2018-02-19 12:23:47 -0500

    Test

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var postcss = require('gulp-postcss');
+var tailwindcss = require('tailwindcss');
+var purgecss = require('gulp-purgecss');
+
+gulp.task('purgecss', function () {
+
+    return gulp.src(['./main.css'])
+    
+        .pipe(postcss([
+            tailwindcss('./tailwind.js'),
+            require('autoprefixer')
+        ]))
+        .pipe(purgecss({
+            content: ["index.html"],
+            extractor: class TailwindExtractor {
+                static extract(content) {
+                    log("call")
+                    log(content.match(/[A-z0-9-:\/]+/g))
+                    return content.match(/[A-z0-9-:\/]+/g) || [];
+                }
+              }
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(concat('purge.min.css'))
+        .pipe(gulp.dest('./build/css'));
+});
+
+
+gulp.task('notpurgecss', function () {
+
+    return gulp.src(['./main.css'])
+        .pipe(postcss([
+            tailwindcss('./tailwind.js'),
+            require('autoprefixer'),
+        ]))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(concat('not-purge.min.css'))
+        .pipe(gulp.dest('./build/css'));
+});
+
+
+gulp.task('html', function () {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('default', ['html', 'notpurgecss', 'purgecss'], function(){});
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Run node server.js to start server from /build
+
+#Run gulp to run the default gulp task
